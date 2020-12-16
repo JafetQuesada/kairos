7536c941d1503e2dae8b860769e75b97b9274e9e
JafetQuesada <73149973+JafetQuesada@users.noreply.github.com>
2020-12-16 14:48:59 -0600

    Add files via upload

--- routes/correo.route.js
+++ routes/correo.route.js
@@ -6,7 +6,7 @@ const mailer = require('../routes/mail');
 
 router.post('/enviar-correo', (req, res) => {
     let body = req.body;
-
+    mailer.enviar_pedido(body.nombre, body.correo, body.telefono, body.descripcion);
     mailer.enviar_mail(body.nombre, body.correo, body.telefono, body.descripcion);
 
     res.json({
--- routes/mail.js
+++ routes/mail.js
@@ -1,6 +1,47 @@
 'use strict'
 const nodemailer = require('nodemailer');
 require('dotenv').config();
+this.enviar_pedido = (nombre, correo, telefono, descripcion) => {
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.MAILUSER,
+            pass: process.env.MAILPSSWD
+        }
+    });
+    let mail_options = {
+        from: `${correo}`,
+        to: 'alimentosKairosymas@gmail.com',
+        subject: 'Confirmación de pedido',
+        html: `
+            <table border="0" cellpadding="0" cellspacing="0" width="600px" background-color="#dd969c" bgcolor="#dd969c">
+                <tr height="200px">  
+                    <td bgcolor="" width="600px">
+                        <h1 style="color: #4b2c21; text-align:center">Pedido de</h1>
+                        <p  style="color: #4b2c21; text-align:center">
+                            <span style="color: #fff">${nombre}</span> 
+                        </p>
+                        <p style="color: #4b2c21; text-align:center">Descripción del pedido: ${descripcion}</p>
+                    </td>
+                </tr>
+                <tr bgcolor="#fff">
+                    <td style="text-align:center">
+                        <p style="color: #4b2c21">Contacto de teléfono ${telefono}</p>
+                    </td>
+                </tr>
+            </table>
+        `
+    };
+    transporter.sendMail(mail_options, (error, info) => {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('El correo se envío correctamente ' + info.response);
+        }
+    });
+};
+
+
 this.enviar_mail = (nombre, correo, telefono, descripcion) => {
     let transporter = nodemailer.createTransport({
         service: 'gmail',
